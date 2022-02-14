a1873567efe2f68e616b5e7ce44ebfb04bf14ad8
Ahmad Raza <raza.ataki@gmail.com>
2022-02-14 17:38:53 +0500

    update database class

--- admin/includes/database.php
+++ admin/includes/database.php
@@ -4,21 +4,26 @@ require_once "new_config.php";
 
 class Database
 {
-    private $connection;
+    public $connection;
+
+    public function __construct()
+    {
+        $this->open_db_connection();
+    }
 
     public function open_db_connection()
     {
 
         $this->connection = mysqli_connect(DB_HOST, DB_USER, DB_PASS, DB_NAME);
 
-        if (!$this->connection) {
-            die("DataBase Connection Failed");
-        } else {
-            echo "connected";
+        if (mysqli_connect_errno()) {
+            die("DataBase Connection Failed Badly " . mysqli_error($this->connection));
         }
+        // else {
+        //     echo "connected";
+        // }
 
     }
 }
 
 $database = new Database();
-$database->open_db_connection();
