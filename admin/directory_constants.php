079b19999bf66ba605b143f4df49b207b6f91605
Ahmad Raza <raza.ataki@gmail.com>
2022-02-15 18:43:42 +0500

    Add PHP Constants

---
+++ admin/directory_constants.php
@@ -0,0 +1,33 @@
+<?php
+
+echo __FILE__ . "<br>";
+echo __LINE__ . "<br>";
+echo __DIR__ . "<br>";
+
+if (file_exists(__DIR__)) {
+    echo "1.Yes" . "<br>";
+}
+
+if (is_file(__FILE__)) {
+    echo "2.Yes" . "<br>";
+} else {
+    echo "2.No" . "<br>";
+}
+
+if (is_file(__DIR__)) {
+    echo "3.Yes" . "<br>";
+} else {
+    echo "3.No" . "<br>";
+}
+
+if (is_dir(__DIR__)) {
+    echo "4.Yes" . "<br>";
+} else {
+    echo "4.No" . "<br>";
+}
+
+if (is_dir(__FILE__)) {
+    echo "5.Yes" . "<br>";
+} else {
+    echo "5.No" . "<br>";
+}
