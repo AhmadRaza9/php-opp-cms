d838ee50bd2bee66cd99cbc596d7bceff6d3f83c
Ahmad Raza <raza.ataki@gmail.com>
2022-02-15 11:25:43 +0500

    Create Backup Function

---
+++ admin/includes/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function autoload($class)
+{
+    $class = strtolower($class);
+    $path = "includes/{$class}.php";
+
+    if (file_exists($path)) {
+        require_once $path;
+    } else {
+        die("This file name {$class}.php was not found...");
+    }
+}
+
+spl_autoload_register('autoload');
