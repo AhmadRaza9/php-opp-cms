a6327381784c7af8ddc7030c867aae42734dec8c
Ahmad Raza <raza.ataki@gmail.com>
2022-02-15 11:13:07 +0500

    Using our instantiation method to find 1 user

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -20,10 +20,13 @@
 // $user = User::instantiation($found_user);
 // var_dump($user);
 
-$users = User::find_all_users();
-foreach ($users as $user) {
-    echo $user->username . "<br>";
-}
+// $users = User::find_all_users();
+// foreach ($users as $user) {
+//     echo $user->username . "<br>";
+// }
+
+$found_user = User::find_user_by_id(2);
+echo $found_user->username
 
 ?>
                         <ol class="breadcrumb">
