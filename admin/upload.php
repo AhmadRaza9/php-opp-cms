98c77dd67279804df307d566592b4b8992a6c4b7
Ahmad Raza <raza.ataki@gmail.com>
2022-02-16 21:31:57 +0500

    Creat the HTML FORM

--- admin/upload.php
+++ admin/upload.php
@@ -1,24 +1,16 @@
 <?php include "includes/header.php";?>
+<?php if (!$session->is_signed_in()) {redirect("login.php");}?>
 
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
-
-
-        <?php include "includes/top_nav.php";?>
-
-
-
-            <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
-
-            <?php include "includes/sidebar.php";?>
-
+            <?php include "includes/top_nav.php";?>
+                <!-- Sidebar Menu Items - These collapse to the responsive navigation menu on small screens -->
+                <?php include "includes/sidebar.php";?>
             <!-- /.navbar-collapse -->
         </nav>
 
         <div id="page-wrapper">
-
             <div class="container-fluid">
-
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
@@ -34,15 +26,22 @@
                                 <i class="fa fa-file"></i> upload
                             </li>
                         </ol>
+                        <div class="col-md-4">
+                            <form action="upload.php" method="POST" enctype="multipart/form-data">
+                                <div class="form-group">
+                                    <input type="text" name="title" class="form-control" placeholder="Enter Image Title">
+                                </div>
+                                <div class="form-group">
+                                    <input type="file" name="file_upload" class="form-control">
+                                </div>
+                                <input type="submit" name="submit" class="btn-primary">
+                            </form>
+                        </div>
                     </div>
                 </div>
                 <!-- /.row -->
-
             </div>
             <!-- /.container-fluid -->
-
-
         </div>
         <!-- /#page-wrapper -->
-
   <?php include "includes/footer.php";?>
