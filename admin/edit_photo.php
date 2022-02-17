32ebdaeeef8042cf6c6bd14c39d40be568e1cb69
Ahmad Raza <raza.ataki@gmail.com>
2022-02-17 18:39:18 +0500

    Creating The Edit Page Part 2

--- admin/edit_photo.php
+++ admin/edit_photo.php
@@ -3,7 +3,8 @@
 <?php if (!$session->is_signed_in()) {redirect("login.php");}?>
 
 <?php
-
+if (isset($_POST['update'])) {
+}
 ?>
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -40,65 +41,61 @@
                             </li>
                         </ol>
 
+                        <form action="edit_photo.php" method="POST" >
+                            <div class="col-md-8">
+                                    <div class="form-group">
+                                        <label for="title">Title</label>
+                                        <input type="text" name="title" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="caption">Caption</label>
+                                        <input type="text" name="caption" class="form-control">
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="alternate_text">Alternate Text</label>
+                                        <input type="text" name="alternate_text" class="form-control">
+                                    </div>
 
-                        <div class="col-md-8">
-                            <form action="edit_photo.php" method="POST" enctype="multipart/form-data">
-
-                                <div class="form-group">
-                                    <label for="title">Title</label>
-                                    <input type="text" name="title" class="form-control">
+                                    <div class="form-group">
+                                        <label for="description">Description</label>
+                                        <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="caption">Caption</label>
-                                    <input type="text" name="caption" class="form-control">
+                                <div class="col-md-4" >
+                                    <div  class="photo-info-box">
+                                        <div class="info-box-header">
+                                        <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
+                                        </div>
+                                        <div class="inside">
+                                            <div class="box-inner">
+                                                <p class="text">
+                                                    <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
+                                                </p>
+                                                <p class="text ">
+                                                    Photo Id: <span class="data photo_id_box">34</span>
+                                                </p>
+                                                <p class="text">
+                                                    Filename: <span class="data">image.jpg</span>
+                                                </p>
+                                                <p class="text">
+                                                    File Type: <span class="data">JPG</span>
+                                                </p>
+                                                <p class="text">
+                                                    File Size: <span class="data">3245345</span>
+                                                </p>
+                                            </div>
+                                            <div class="info-box-footer clearfix">
+                                                <div class="info-box-delete pull-left">
+                                                    <a  href="delete_photo.php?id=1" class="btn btn-danger btn-lg ">Delete</a>
+                                                </div>
+                                                <div class="info-box-update pull-right ">
+                                                    <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
                                 </div>
-                                <div class="form-group">
-                                    <label for="alternate_text">Alternate Text</label>
-                                    <input type="text" name="alternate_text" class="form-control">
-                                </div>
-
-                                <div class="form-group">
-                                    <label for="description">Description</label>
-                                    <textarea name="description" id="" cols="30" rows="10" class="form-control"></textarea>
-                                </div>
-
-
                             </form>
-                        </div>
-                            <div class="col-md-4" >
-                                <div  class="photo-info-box">
-                                    <div class="info-box-header">
-                                    <h4>Save <span id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span></h4>
-                                    </div>
-                                    <div class="inside">
-                                        <div class="box-inner">
-                                            <p class="text">
-                                            <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
-                                            </p>
-                                            <p class="text ">
-                                                Photo Id: <span class="data photo_id_box">34</span>
-                                            </p>
-                                            <p class="text">
-                                                Filename: <span class="data">image.jpg</span>
-                                            </p>
-                                            <p class="text">
-                                            File Type: <span class="data">JPG</span>
-                                            </p>
-                                            <p class="text">
-                                            File Size: <span class="data">3245345</span>
-                                            </p>
-                                        </div>
-                                        <div class="info-box-footer clearfix">
-                                            <div class="info-box-delete pull-left">
-                                                <a  href="delete_photo.php?id=1" class="btn btn-danger btn-lg ">Delete</a>
-                                            </div>
-                                            <div class="info-box-update pull-right ">
-                                                <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
-                                            </div>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
 
 
 
