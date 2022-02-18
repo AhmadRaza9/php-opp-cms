5658242d68e9fd022adb2ee799590796857a0047
Ahmad Raza <raza.ataki@gmail.com>
2022-02-18 20:15:20 +0500

    Creating the Comment Class

---
+++ admin/includes/comment.php
@@ -0,0 +1,12 @@
+<?php
+
+class Comment extends Db_object
+{
+    protected static $db_table = "comments";
+    protected static $db_table_field = array('id', 'photo_id', 'author', 'body');
+    public $id;
+    public $photo_id;
+    public $author;
+    public $body;
+
+} // End of Class Comment
