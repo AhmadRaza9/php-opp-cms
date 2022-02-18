20ae25441f931f98a30338b8033f68790b181c81
Ahmad Raza <raza.ataki@gmail.com>
2022-02-18 11:52:33 +0500

    Displaying User Page

--- admin/users.php
+++ admin/users.php
@@ -1,5 +1,12 @@
 <?php include "includes/header.php";?>
 
+<?php if (!$session->is_signed_in()) {redirect("login.php");}?>
+
+<?php
+
+$users = User::find_all();
+
+?>
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
 
@@ -34,6 +41,45 @@
                                 <i class="fa fa-file"></i> users
                             </li>
                         </ol>
+
+
+                        <div class="col-md-12">
+
+                            <table class="table table-hover">
+                                <thead>
+                                    <tr>
+                                        <th>Id</th>
+                                        <th>Photo</th>
+                                        <th>Username</th>
+                                        <th>First Name</th>
+                                        <th>Last Name</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php foreach ($users as $user): ?>
+                                    <tr>
+                                        <td><?php echo $user->id; ?></td>
+                                        <td><img style="width:100px;" src= "images/placeholder.png"></td>
+                                        <td>
+                                            <?php echo $user->username; ?>
+                                            <div class="pictures_links">
+                                                <a href="delete_user.php?id=<?php echo $user->id; ?>">Delete</a>
+                                                <a href="edit_user.php?id=<?php echo $user->id; ?>">Edit</a>
+                                                <a href="">View</a>
+                                            </div>
+                                        </td>
+                                        <td><?php echo $user->first_name; ?></td>
+                                        <td><?php echo $user->last_name; ?></td>
+                                    </tr>
+                                    <?php endforeach;?>
+                                </tbody>
+                            </table>
+
+                        </div>
+
+
+
+
                     </div>
                 </div>
                 <!-- /.row -->
