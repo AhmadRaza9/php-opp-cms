6de529f172c357c549bda9fd9b58f5c7ad1f836e
Ahmad Raza <raza.ataki@gmail.com>
2022-02-18 15:31:15 +0500

    Update add user page

--- admin/add_user.php
+++ admin/add_user.php
@@ -4,6 +4,10 @@
 
 <?php
 
+if (isset($_POST['submit'])) {
+    echo "yes its works";
+}
+
 ?>
         <!-- Navigation -->
         <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
@@ -32,9 +36,9 @@
                         </h1>
 
                         <form method="POST" >
-                            <div class="col-md-8">
+                            <div class="col-md-4">
                                     <div class="form-group">
-                                        <label for="username">username</label>
+                                        <label for="username">Username</label>
                                         <input type="text" name="username" class="form-control" placeholder="Enter Username">
                                     </div>
                                     <div class="form-group">
@@ -49,47 +53,11 @@
                                         <label for="password">Password</label>
                                         <input type="password" name="password" class="form-control" placeholder="Enter Password">
                                     </div>
-                                </div>
-                                <div class="col-md-4" >
-                                    <div  class="photo-info-box">
-                                        <div class="info-box-header">
-                                            <h4>Save</h4>
-                                            <span data-toggle="collapse" data-target="#demo" id="toggle" class="glyphicon glyphicon-menu-up pull-right"></span>
-                                        </div>
-                                        <div class="inside collapse in" id="demo">
-                                            <div class="box-inner">
-                                                <p class="text">
-                                                    <span class="glyphicon glyphicon-calendar"></span> Uploaded on: April 22, 2030 @ 5:26
-                                                </p>
-                                                <p class="text ">
-                                                    User Id: <span class="data photo_id_box"></span>
-                                                </p>
-                                                <p class="text">
-                                                    Username: <span class="data"></span>
-                                                </p>
-                                                <p class="text">
-                                                    First Name: <span class="data"></span>
-                                                </p>
-                                                <p class="text">
-                                                    Last Name: <span class="data"></span>
-                                                </p>
-                                            </div>
-                                            <div class="info-box-footer clearfix">
-                                                <div class="info-box-delete pull-left">
-                                                    <a  href="delete_photo.php?id=1" class="btn btn-danger btn-lg ">Delete</a>
-                                                </div>
-                                                <div class="info-box-update pull-right ">
-                                                    <input type="submit" name="update" value="Update" class="btn btn-primary btn-lg ">
-                                                </div>
-                                            </div>
-                                        </div>
+                                    <div class="form-group">
+                                        <input type="submit" name="submit" class="btn btn-primary pull-right">
                                     </div>
                                 </div>
                             </form>
-
-
-
-
                     </div>
                 </div>
                 <!-- /.row -->
