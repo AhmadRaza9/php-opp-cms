5fa09a0f9e964f053dc2d96e5da0c054e5ed9189
Ahmad Raza <raza.ataki@gmail.com>
2022-02-19 17:46:00 +0500

    Dashboard HTML Snippets Inclusion

--- admin/includes/admin_content.php
+++ admin/includes/admin_content.php
@@ -1,5 +1,4 @@
             <div class="container-fluid">
-
                 <!-- Page Heading -->
                 <div class="row">
                     <div class="col-lg-12">
@@ -7,60 +6,103 @@
                             Admin
                             <small>Subheadings</small>
                         </h1>
-<?php
-
-// $user = new User();
-// $user->username = "username";
-// $user->password = "password";
-// $user->first_name = "first_name";
-// $user->last_name = "last_name";
-
-// $user->create();
-
-// $user = User::find_by_id(1);
-// echo $user->username;
-// $user->update();
-
-// $user->save();
-// $user->delete();
-
-// $users = User::find_all();
-// foreach ($users as $user) {
-//     echo $user->username;
-// }
-
-// $photo = new Photo();
-// $photo->title = "test";
-// $photo->description = "test description";
-// $photo->filename = "test filename";
-// $photo->type = "test type";
-// $photo->size = "1";
-
-// $photo->create();
-
-// $photos = Photo::find_by_id(1);
-// foreach ($photos as $photo) {
-//     var_dump($photo);
-// }
-
-// $photos = Photo::find_by_id(9);
-
-// echo $photos->title;
-
-// echo INCLUDES_PATH;
-
-?>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.html">Dashboard</a>
-                            </li>
-                            <!-- <li class="active">
-                                <i class="fa fa-file"></i>
-                            </li> -->
-                        </ol>
                     </div>
                 </div>
-                <!-- /.row -->
+                <div class="row">
+                    <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-primary">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-users fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">4</div>
+                                        <div>New Views</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">View Details</span>
+                                <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
 
+                        <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-green">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-photo fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">7</div>
+                                        <div>Photos</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Photos in Gallery</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+
+                        <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-yellow">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-user fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">8
+
+                                        </div>
+
+                                        <div>Users</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Users</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+
+                        <div class="col-lg-3 col-md-6">
+                        <div class="panel panel-red">
+                            <div class="panel-heading">
+                                <div class="row">
+                                    <div class="col-xs-3">
+                                        <i class="fa fa-support fa-5x"></i>
+                                    </div>
+                                    <div class="col-xs-9 text-right">
+                                        <div class="huge">8</div>
+                                        <div>Comments</div>
+                                    </div>
+                                </div>
+                            </div>
+                            <a href="#">
+                                <div class="panel-footer">
+                                    <span class="pull-left">Total Comments</span>
+                                    <span class="pull-right"><i class="fa fa-arrow-circle-right"></i></span>
+                                    <div class="clearfix"></div>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                </div>
             </div>
-            <!-- /.container-fluid -->
