2a829154269e31cf523dd29f74c078a9da9b67ec
Ahmad Raza <raza.ataki@gmail.com>
2022-02-21 15:02:39 +0500

    Add pagination to admin/photos.php

--- admin/photos.php
+++ admin/photos.php
@@ -4,7 +4,15 @@
 
 <?php
 
-$photos = Photo::find_all();
+$page = !empty($_GET['page']) ? (int) $_GET['page'] : 1;
+$items_per_page = 4;
+$items_total_count = Photo::count_all();
+
+$paginate = new Paginate($page, $items_per_page, $items_total_count);
+
+$sql = "SELECT * FROM photos LIMIT {$items_per_page} OFFSET {$paginate->offset()}";
+
+$photos = Photo::find_by_query($sql);
 
 ?>
         <!-- Navigation -->
@@ -34,16 +42,27 @@ $photos = Photo::find_all();
                             Photos
                             <small>Subheading</small>
                         </h1>
-                        <ol class="breadcrumb">
-                            <li>
-                                <i class="fa fa-dashboard"></i>  <a href="index.php">Dashboard</a>
-                            </li>
-                            <li class="active">
-                                <i class="fa fa-file"></i> photos
-                            </li>
-                        </ol>
+                <ul class="pager">
 
+                    <?php if ($paginate->page_total() > 1): ?>
+                        <?php if ($paginate->has_next()): ?>
+                            <li class="next"><a href="?page=<?php echo $paginate->next(); ?>">Next</a></li>
+                        <?php endif;?>
+                    <?php endif;?>
 
+                    <?php for ($i = 1; $i <= $paginate->page_total(); $i++): ?>
+                        <?php if ($i == $paginate->current_page) {
+    echo "<li><a class='bg-active' href='?page={$i}'>${i}</a></li>";
+} else {
+    echo "<li><a href='?page={$i}'>${i}</a></li>";
+}?>
+                    <?php endfor;?>
+
+                    <?php if ($paginate->has_previous()): ?>
+                        <li class="previous"><a href="?page=<?php echo $paginate->previous(); ?>">Previous</a></li>
+                    <?php endif;?>
+
+                </ul>
                         <div class="col-md-12">
 
                             <table class="table table-hover">
